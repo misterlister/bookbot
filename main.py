4c8957f1030bfb293d71478e4352f1c7d036cd3e
Hayden Lister <haydenglister@gmail.com>
2024-02-25 22:01:57 -0800

    Added top word count to report, split make_report into separate functions

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+max_top_words = 100
+lg_border = "---"
+sm_border = "-"
+
 def main():
     book_path = get_book_path()
     text = get_book_text(book_path)
@@ -5,7 +9,7 @@ def main():
         make_report(book_path, text)
 
 def get_book_path():
-    path = input("Please enter the path to the book you wish to analyze (eg. books/frankenstein.txt): ")
+    path = input("Please enter the path to the book you wish to analyze (eg. books/Frankenstein.txt): ")
     return path
 
 def get_book_text(path):
@@ -20,6 +24,20 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+def count_of_each_word(text):
+    words = text.split()
+    word_count = {}
+    for word in words:
+        first_letter = word[0].lower()
+        if len(word) > 1:
+            entry = first_letter + word[1:]
+        else:
+            entry = first_letter
+        if entry not in word_count:
+            word_count[entry] = 0
+        word_count[entry] += 1
+    return word_count
+
 def count_letters(text):
     letter_count = {}
     for letter in text:
@@ -28,21 +46,54 @@ def count_letters(text):
             letter_count[lower_letter] = 0
         letter_count[lower_letter] += 1
     return letter_count
+
+def sorted_dict(dictionary):
+    return dict(sorted(dictionary.items(), key = lambda x:x[1], reverse = True))
+
+def filter_top_words(dictionary, filter_number):
+    word_list = []
+    actual_filter_number = 0
+    for word in dictionary:
+        word_list.append(word)
+        word_list.append(dictionary[word])
+        actual_filter_number += 1
+        if len(word_list) >= (filter_number * 2):
+            return word_list, actual_filter_number
+    return word_list, actual_filter_number
+
+def filter_non_alpha(dictionary) -> list:
+    alpha_list = []
+    for character in dictionary:
+        if character.isalpha():
+            alpha_list.append(character)
+            alpha_list.append(dictionary[character])
+    return alpha_list
+
+def format_count(count_list, count_type) -> str:
+    formatted = ""
+    for i in range (0, len(count_list), 2):
+        formatted += f"The {count_type} '{count_list[i]}' appears {count_list[i+1]} times\n"
+    return formatted
+
 def make_report(path, text):
     num_words = get_num_words(text)
-    character_count = count_letters(text)
-    sorted_count = dict(sorted(character_count.items(), key = lambda x:x[1], reverse = True))
-    letter_count = []
-    for character in sorted_count:
-        if character.isalpha():
-            letter_count.append(character)
-            letter_count.append(sorted_count[character])
-    count_formatted = ""
-    for i in range (0, len(letter_count), 2):
-        count_formatted += f"The character '{letter_count[i]}' appears {letter_count[i+1]} times\n"
-    print(f"\n--- Report of {path} ---\n")
-    print(f"There are {num_words} words in this document")
-    print(count_formatted)
-    print("--- End of report ---\n")
+
+    all_words_count = count_of_each_word(text)
+    sorted_words = sorted_dict(all_words_count)
+    top_words, top_word_num = filter_top_words(sorted_words, max_top_words)
+    words_formatted = format_count(top_words, "word")
+
+    all_character_count = count_letters(text)
+    sorted_count = sorted_dict(all_character_count)
+    letter_count = filter_non_alpha(sorted_count)
+    letter_formatted = format_count(letter_count, "character")
+
+    print(f"\n{lg_border} Report of {path} {lg_border}\n")
+    print(f"There are {num_words} words in this document\n")
+    print(f"{sm_border} Top {top_word_num} words in this document {sm_border}\n")
+    print(words_formatted)
+    print(f"{sm_border} Character count {sm_border}\n")
+    print(letter_formatted)
+    print(f"{lg_border} End of report {lg_border}\n")
     
 main()
